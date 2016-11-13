83f333d3cc46c1e108115c0a93e3fbb4750a3360
CKutsenko <kate040491@mail.ru>
2016-11-13 23:25:11 +0300

    Update 2016-11-13T20:25:06.891Z

--- js/script.js
+++ js/script.js
@@ -10227,68 +10227,97 @@ window.slick =              require('./vendor/bower/slick');
 jQuery(document).ready(function($){
 
   /* Hamburger */
-  $('.hamburger').click(function(e){
+var toggler = document.getElementById('hamburger');
+toggler.onclick = function(e){
+  e.preventDefault();
+  toggler.classList.toggle('hamburger--close');
+  document.getElementById('nav__list').classList.toggle('nav__list--visible');
+}
+
+
+  $('.aside__btn').click(function(e){
     e.preventDefault();
-    $('.nav__list-wrapper').toggle();
+    if($(this).next('.aside__accordion').is(":visible")) {
+      $(this).next('.aside__accordion').hide().addClass('active');
+    } else {
+      $(this).closest('.aside').find('.aside__accordion').hide();
+      $(this).next('.aside__accordion').show().removeClass('active').addClass('none');
+    }
   });
 
-  /* City list */
-  $('.city__name').click(function(){
-    $('.city__dropdown').toggle();
-  });
-
-
-  /* галерея Gratitude */
-  $('.promo-slider').slick({
+    $('.promo-slider').slick({
     infinite: true,
     arrows: false,
     dots: true,
     slidesToShow: 1,
-    slidesToScroll: 1,
-    centerMode: true
+    slidesToScroll: 1
   });
-  /* Gratitude in the modal window */
-  $('.gratitude__link').click( function(e){
-    e.preventDefault();
-    $('body').css({"overflow":"hidden"});
-    $('.overlay').show();
-    $(this).closest('.gratitude__slide').find('.gratitude__modal').clone().appendTo($('.overlay'))
-    .show()
-    .animate({opacity: 1}, 200);
+
+
+$('.card__tabs-link a').on('click', function(e){
+  e.preventDefault();
+
+  $(this)
+    .closest('.card__tabs-list')
+    .find('.card__tabs-link.active')
+    .removeClass('active');
+  $(this)
+   .closest('.card__tabs-link')
+   .addClass('active');
+
+  $(this)
+   .closest('.card__tabs')
+   .find('.card__tab-item.active')
+   .removeClass('active');
+  link_href = $(this).attr('href');
+  $(link_href).addClass('active');
+});
+
+  /* Модальное окно "Заказать звонок" */
+  $('.header__btn').click( function(event){ // лoвим клик пo ссылки с id="go"
+    event.preventDefault();
+    $('body').css({"overflow":"hidden"});// выключaем стaндaртную рoль элементa
+    $('.header__overlay').fadeIn(400, // снaчaлa плaвнo пoкaзывaем темную пoдлoжку
+      function(){ // пoсле выпoлнения предъидущей aнимaции
+        $('.form')
+          .css('display', 'block') // убирaем у мoдaльнoгo oкнa display: none;
+          .animate({opacity: 1, top: '20%'}, 200); // плaвнo прибaвляем прoзрaчнoсть oднoвременнo сo съезжaнием вниз
+    });
   });
-  /* Close the modal window */
-  $('.overlay').click( function(){
+  /* Зaкрытие мoдaльнoгo oкнa, тут делaем тo же сaмoе нo в oбрaтнoм пoрядке */
+  $('.popup__bgr').click( function(){ // лoвим клик пo крестику или пoдлoжке
     $('body').css({"overflow":"auto"});
-    $(this).find('.gratitude__modal')
-      .animate({opacity: 0}, 200,
-        function(){
-          $(this).remove();
-          $('.overlay').fadeOut(400);
+    $('.form')
+      .animate({opacity: 0, top: '45%'}, 200,  // плaвнo меняем прoзрaчнoсть нa 0 и oднoвременнo двигaем oкнo вверх
+        function(){ // пoсле aнимaции
+          $(this).css('display', 'none'); // делaем ему display: none;
+          $('.header__overlay').fadeOut(400); // скрывaем пoдлoжку
         }
       );
   });
 
-  /* Gratitude in the modal window */
-  $('.reviews__link').click( function(e){
-    e.preventDefault();
-    $('body').css({"overflow":"hidden"});
-    $('.overlay').show();
-    $(this).closest('.reviews__slide').find('.reviews__modal').clone().appendTo($('.overlay'))
-    .show()
-    .animate({opacity: 1}, 200);
+  /* Модальное окно "Заказать звонок" */
+  $('.card__btn').click( function(event){ // лoвим клик пo ссылки с id="go"
+    event.preventDefault();
+    $('body').css({"overflow":"hidden"});// выключaем стaндaртную рoль элементa
+    $('.card__overlay').fadeIn(400, // снaчaлa плaвнo пoкaзывaем темную пoдлoжку
+      function(){ // пoсле выпoлнения предъидущей aнимaции
+        $('.form')
+          .css('display', 'block') // убирaем у мoдaльнoгo oкнa display: none;
+          .animate({opacity: 1, top: '10%'}, 200); // плaвнo прибaвляем прoзрaчнoсть oднoвременнo сo съезжaнием вниз
+    });
   });
-  /* Close the modal window */
-  $('.overlay').click( function(){
+  /* Зaкрытие мoдaльнoгo oкнa, тут делaем тo же сaмoе нo в oбрaтнoм пoрядке */
+  $('.popup__bgr').click( function(){ // лoвим клик пo крестику или пoдлoжке
     $('body').css({"overflow":"auto"});
-    $(this).find('.reviews__modal')
-      .animate({opacity: 0}, 200,
-        function(){
-          $(this).remove();
-          $('.overlay').fadeOut(400);
+    $('.form')
+      .animate({opacity: 0, top: '45%'}, 200,  // плaвнo меняем прoзрaчнoсть нa 0 и oднoвременнo двигaем oкнo вверх
+        function(){ // пoсле aнимaции
+          $(this).css('display', 'none'); // делaем ему display: none;
+          $('.card__overlay').fadeOut(400); // скрывaем пoдлoжку
         }
       );
   });
-
 
 });
 
